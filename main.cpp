78d35242957f645ffd55d2b13e4b9ceaa8c7e011
Casey Beach <beachc@gmail.com>
2010-10-13 22:51:04 -0700

    changed filename from SPH.cpp to main.cpp

---
+++ main.cpp
@@ -0,0 +1,25 @@
+// SPH.cpp : Defines the entry point for the console application.
+//
+
+//#include "stdafx.h"
+
+#include <util/uVect.h>
+#include <iostream>
+
+#include <render/ogl.h>
+#include <particle/sp.h>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	uVect *vect = new uVect(1,0,0,SPHERICAL);
+	ogl sph;
+
+//	sph.Start(argc, argv);
+	SmoothedParticle sp;
+	
+
+	return 0;
+}
+
